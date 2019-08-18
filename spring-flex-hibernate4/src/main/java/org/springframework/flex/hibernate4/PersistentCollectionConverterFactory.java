ce8b4df612a5cda82de67d8a17a81be87ce1f8e4
kavyasrid <53416878+kavyasrid@users.noreply.github.com>
2019-08-18 16:35:01 -0400

    Update PersistentCollectionConverterFactory.java

--- spring-flex-hibernate4/src/main/java/org/springframework/flex/hibernate4/PersistentCollectionConverterFactory.java
+++ spring-flex-hibernate4/src/main/java/org/springframework/flex/hibernate4/PersistentCollectionConverterFactory.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2002-2011 the original author or authors.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.flex.hibernate4;
 
 import org.hibernate.Hibernate;
@@ -31,7 +15,7 @@ import org.springframework.core.convert.converter.ConverterFactory;
  *     <li>Convert to the wrapped collection class if the {@code PersistentCollection} is initialized</li> 
  * </ul>
  *
- * @author Jeremy Grelle
+ * 
  */
 public class PersistentCollectionConverterFactory implements ConverterFactory<PersistentCollection, Object> {
 
